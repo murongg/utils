1940783f3fa99a3b8cf4918d4644dd11d3a0cc45
MuRong <372638156@qq.com>
2023-11-29 09:57:58 +0800

    feat!: NullableObjectWithKey rename to NullableObjectWithKeys

--- src/types.ts
+++ src/types.ts
@@ -11,9 +11,10 @@ export type NullableObject<T extends Object> = {
  * Array value null or whatever
  */
 export type NullableArray<T> = Array<Nullable<T>>
+
 /**
  * Object value null or whatever with keys
  */
-export type NullableObjectWithKey<T extends Object, Key extends keyof T > = {
+export type NullableObjectWithKeys<T extends Object, Key extends keyof T> = {
   [P in keyof T]: P extends Key ? Nullable<T[P]> : T[P]
 }

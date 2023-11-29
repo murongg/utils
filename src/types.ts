de274dd237ff002ccb516fa5ef4b92038856968d
MuRong <372638156@qq.com>
2023-11-29 11:03:27 +0800

    fix: NullableArray

--- src/types.ts
+++ src/types.ts
@@ -9,11 +9,15 @@ export type NullableObject<T extends Object> = {
 
 /**
  * Array value null or whatever
+ * @example NullableArray<string[]>
  */
-export type NullableArray<T> = Array<Nullable<T>>
+export type NullableArray<T> = {
+  [P in keyof T]: Nullable<T[P]>
+}
 
 /**
  * Object value null or whatever with keys
+ * @example NullableObjectWithKeys<Record<'a' | 'b', string>, 'a'>
  */
 export type NullableObjectWithKeys<T extends Object, Key extends keyof T> = {
   [P in keyof T]: P extends Key ? Nullable<T[P]> : T[P]

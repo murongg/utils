ef10a6795131b0a825284390372992fd0b552a0b
MuRong <372638156@qq.com>
2023-11-29 10:26:27 +0800

    chore: add js doc

--- src/to.ts
+++ src/to.ts
@@ -1,5 +1,10 @@
 import { isMaybeNumber } from './is'
 
+/**
+ * Convert a value to a number.
+ * @param val
+ * @returns
+ */
 export function toNumber(val: any) {
   return isMaybeNumber(val) ? Number(val) : 0
 }

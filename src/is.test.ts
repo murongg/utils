38cfdaae2925d6c0293a37f67836a00e0f88b281
MuRong <372638156@qq.com>
2023-11-29 09:51:48 +0800

    feat: isStringNumber

--- src/is.test.ts
+++ src/is.test.ts
@@ -1,5 +1,5 @@
 import { expect, it } from 'vitest'
-import { isMaybeNumber } from './is'
+import { isMaybeNumber, isStringNumber } from './is'
 
 it('isMaybeNumber', () => {
   expect(isMaybeNumber(1)).toBeTruthy()
@@ -7,4 +7,11 @@ it('isMaybeNumber', () => {
   expect(isMaybeNumber('0')).toBeTruthy()
   expect(isMaybeNumber('0a')).toBeFalsy()
   expect(isMaybeNumber('asb89*')).toBeFalsy()
+})
+
+it('isStringNumber', () => {
+  expect(isStringNumber('1')).toBeTruthy()
+  expect(isStringNumber('0')).toBeTruthy()
+  expect(isStringNumber('0a')).toBeFalsy()
+  expect(isStringNumber('asb89*')).toBeFalsy()
 })
--- src/is.ts
+++ src/is.ts
@@ -1,10 +1,19 @@
 import { isNumber } from '@antfu/utils'
 
 /**
+ * Check if the value is a string of number
+ * @param val
+ * @returns
+ */
+export function isStringNumber(val: string): boolean {
+  return /^\d+$/.test(val)
+}
+
+/**
  * Check if the value is a number or a string of number
  * @param val
  * @returns
  */
 export function isMaybeNumber(val: any): boolean {
-  return isNumber(val) || /^\d+$/.test(val)
+  return isNumber(val) || isStringNumber(val)
 }

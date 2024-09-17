db22408c61ee2355fca265e1644ca67d54a77251
MuRong <hi@mrong.me>
2024-09-17 10:33:00 +0800

    feat(fn): compose

---
+++ src/fn.ts
@@ -0,0 +1,19 @@
+import type { ArgumentsFn } from '../dist'
+
+/**
+ * Compose functions
+ * @param handles
+ * @param args
+ * @returns
+ */
+export function compose<T>(handles: ArgumentsFn<T>[], args: Array<any> = []) {
+  function dispatch(index: number) {
+    if (index === handles.length)
+      return Promise.resolve()
+
+    const handle = handles[index]
+    return Promise.resolve(handle(...args, () => dispatch(index + 1)))
+  }
+
+  return dispatch(0)
+}
--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export * from './to'
 export * from './regexp_rules'
 export * from './promise'
 export * from './array'
+export * from './fn'
 
 export type * from './types'
